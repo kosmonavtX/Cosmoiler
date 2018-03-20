9aaf70bff79f872f42b4f45a58479ac7be75c868
kosmonavtX <jawaman76@gmail.com>
2018-03-20 21:48:44 +0300

    localization

--- src/store/store.js
+++ src/store/store.js
@@ -82,7 +82,7 @@ const store = new Vuex.Store({
         debug: ["234", 4544],
         connection: ws,
         connect: false,
-        locale: 'en',//window.navigator.userLanguage || window.navigator.language,
+        locale: window.navigator.userLanguage || window.navigator.language,
     },
     mutations: {
         SET_CONFIG (state, payload) {
